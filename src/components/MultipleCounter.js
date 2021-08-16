db52ab9769947ae2cfc4b8de245af4f522693e62
David Salonga <fdc.salonga@gmail.com>
2021-08-16 18:43:26 +0800

    [React-Counter][Francis] feat: implement functionalities

---
+++ src/components/MultipleCounter.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import CounterGroup from "./Countergroup";
+import CounterSizeGenerator from "./CounterSizeGenerator";
+
+function MultipleCounter(){
+    const [counterSize, setCounterSize] = useState(0);
+
+    function updateCounterSize(counterSize){
+        setCounterSize(counterSize);
+    }
+
+    return (
+        <div>
+            <CounterSizeGenerator updateCounterSize={updateCounterSize}></CounterSizeGenerator>
+            <CounterGroup counterSize={counterSize}></CounterGroup>
+        </div>
+    )
+}
+
+export default MultipleCounter;
